62230446da54910b0bba0ddaf051bdda85e762fa
CocoCocoAlx <vania182004@gmail.com>
2022-12-15 21:02:28 -0300

    Modificaciones menores

--- src/main/java/com/polotic/FiestasProvinciales/entidades/Fiesta.java
+++ src/main/java/com/polotic/FiestasProvinciales/entidades/Fiesta.java
@@ -6,6 +6,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
@@ -42,6 +44,7 @@ public class Fiesta{
 
     private String foto;
 
+    @ManyToOne
     private Predio predio;
 
 }
