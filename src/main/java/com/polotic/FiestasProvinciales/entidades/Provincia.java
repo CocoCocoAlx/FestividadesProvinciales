5e355a1d7ce0e38058e0bb16c9deab36721aa8e9
luzmarielabelen <luzespindola256@gmai.com>
2022-12-19 22:37:10 -0300

    verificado

--- src/main/java/com/polotic/FiestasProvinciales/entidades/Provincia.java
+++ src/main/java/com/polotic/FiestasProvinciales/entidades/Provincia.java
@@ -6,9 +6,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
 import java.util.List;
-
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
